0a8d7053e0e6576ce5f8ea43be119e354b18786a
DanieleSalinitro <daniele.salinitro@icloud.com>
2024-04-10 09:39:55 +0200

    modifiche

--- js/script.js
+++ js/script.js
@@ -10,13 +10,14 @@ const { createApp } = Vue;
         data() {
             return {
                 emails: [],
+
+
+
             }
         },
         mounted() {
             for (let i = 0; i < 10; i++) {
-                axios
-                    .get('https://flynn.boolean.careers/exercises/api/random/mail')
-                    .then((response) => {
+                axios.get('https://flynn.boolean.careers/exercises/api/random/mail').then((response) => {
                         this.emails.push(response.data.response);
                     })
             }
