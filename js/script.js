ef71d47beafbb60e3b6b6a8e255c45a08c41a88e
DanieleSalinitro <daniele.salinitro@icloud.com>
2024-04-10 09:18:37 +0200

    modifiche

--- js/script.js
+++ js/script.js
@@ -1,11 +1,25 @@
+/*Attraverso l'apposita API di Boolean
+https://flynn.boolean.careers/exercises/api/random/mail
+generare 10 indirizzi email e stamparli in pagina all'interno di una lista.
+Bonus
+Far comparire gli indirizzi email solamente quando sono stati tutti generati.*/
+
 const { createApp } = Vue;
 
     createApp({
-      data(){
-        return {
-            
+        data() {
+            return {
+                emails: [],
+            }
+        },
+        mounted() {
+            for (let i = 0; i < 10; i++) {
+                axios
+                    .get('https://flynn.boolean.careers/exercises/api/random/mail')
+                    .then((response) => {
+                        this.emails.push(response.data.response);
+                    })
+            }
         }
-      },
-      methods: {
-      }
     }).mount('#app')
+
